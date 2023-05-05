4cb1319f3874e4ebb62cd5c8acbd1769c8ff6230
Ssrx <1342557272@qq.com>
2023-05-05 11:21:52 +0800

    first commit

---
+++ src/main/java/com/srx/git/GitTest1.java
@@ -0,0 +1,12 @@
+package com.srx.git;
+
+/**
+ * @author :srx
+ * @date : 2023/5/5 11:16
+ */
+public class GitTest1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
